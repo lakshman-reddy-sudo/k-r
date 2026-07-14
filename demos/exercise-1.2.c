f722a2da2583db85699d6120fbba25d8d166413a
Lakshman Reddy <144447697+lakshman-reddy-sudo@users.noreply.github.com>
2026-07-14 21:25:45 +0530

    Update comment prefix in exercise-1.2.c
    Changed comment from #A to #Code in the header.

--- demos/exercise-1.2.c
+++ demos/exercise-1.2.c
@@ -1,7 +1,7 @@
 #Q - Experiment to find out what happens when printf's argument
 string contains \c, where c is some character not listed above.
 
-#A - #include <stdio.h>
+#Code - #include <stdio.h>
 
 int main(void) {
     printf("\c");
