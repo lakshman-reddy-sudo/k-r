45288c8b93f74cbc6106ed48de25de9eaa766d41
Lakshman Reddy <144447697+lakshman-reddy-sudo@users.noreply.github.com>
2026-07-14 21:20:09 +0530

    Add exercise instructions for hello world program
    Added instructions for running and experimenting with the 'hello, world' program.

---
+++ demos/exercise-1.1.c
@@ -0,0 +1,8 @@
+#Q - Run the "hello, world" program on your system. Experiment
+with leaving out parts of the program, to see what error messages you get.
+
+"hello, world"
+
+#Error - expected identifier or '('
+    1 | "hello, world"
+      | ^
